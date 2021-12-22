ecb30d915b714b7c6f77c7849b6a5f742eeb059c
Zdravko Danev <zdanev@gmail.com>
2021-12-21 23:31:55 -0800

    day 22, part 1

--- day22/day22.py
+++ day22/day22.py
@@ -4,19 +4,16 @@ with open('day22/input.txt') as f:
     lines = f.readlines()
 
 dic = defaultdict(int)
-i = 0
 for line in lines:
-    i += 1
-    print(i)
-    line = line.replace("on ", "1,").replace("off", "0, ").replace("..", ",") \
+    line = line.replace("on ", "1,").replace("off", "0,").replace("..", ",") \
         .replace("x=", "").replace("y=", "").replace("z=", "")
     
-    q,x1,x2,y1,y2,z1,z2=line.split(",")
+    q, x1, x2, y1, y2, z1, z2 = map(int, line.split(","))
 
-    for x in range(int(x1), int(x2) + 1):
-        for y in range(int(y1), int(y2) + 1):
-            for z in range(int(z1), int(z2) + 1):
-                dic[(x, y, z)] = int(q)
+    for x in range(x1, x2 + 1):
+        for y in range(y1, y2 + 1):
+            for z in range(z1, z2 + 1):
+                dic[(x, y, z)] = q
 
 count = 0
 for key in dic:
